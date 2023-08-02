cc79942e23efd0fb2393d4548e21db3f8962955c
Alexander Martin <hi@droqen.com>
2023-08-02 14:48:44 -0400

    created 'playerboat' ember (no function)

---
+++ BoatGameBlue/embers/boatblue_playerboat/src/client.rs
@@ -0,0 +1,15 @@
+use ambient_api::prelude::*;
+
+// use ambient_api::core::player::components::{player, user_id};
+
+#[main]
+pub fn main() {
+    // spawn_query((player(), user_id())).bind(|players|{
+    //     for (player,(_,uid)) in players {
+    //         // spawn boat.
+    //         // spawn clientside camera. attach to boat.
+    //         // handle input somehow?
+    //     }
+    // });
+    
+}
---
+++ BoatGameBlue/embers/boatblue_playerboat/src/server.rs
@@ -0,0 +1,13 @@
+use ambient_api::prelude::*;
+
+// use ambient_api::core::player::components::{player, user_id};
+
+#[main]
+pub fn main() {
+    // spawn_query((player(), user_id())).bind(|players|{
+    //     for (player,(_,uid)) in players {
+    //         // spawn boat.
+    //     }
+    // });
+    // set boat movement by player cursor?
+}
