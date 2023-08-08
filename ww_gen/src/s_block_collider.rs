e4f676225bd7673706168607f41de58d639e3f0b
Alexander Martin <hi@droqen.com>
2023-08-08 18:17:22 -0400

    setting up ww_gen, lots unused

---
+++ ww_gen/src/s_block_collider.rs
@@ -0,0 +1,70 @@
+use std::f32::consts::PI;
+
+use ambient_api::{
+    core::{
+        primitives::components::cube,
+        transform::components::{translation, rotation, scale},
+        physics::components::cube_collider,
+    },
+    prelude::*,
+};
+use crate::ww_gen::components::{block_size, block_door_side};
+
+pub fn setup() {
+    let collider_height : f32 = 10.; // fixed.
+    let wall_thickness : f32 = 0.2;
+    let visualize : bool = false;
+    spawn_query((translation(), block_size())).bind(move |blocks|for (block, (pos, size)) in blocks {
+        let radius : f32 = size.x.max(size.y);
+        let _vis_height : f32 = size.z; // variable.
+        if let Some(mousehole_side) = entity::get_component(block, block_door_side()) {
+            spawn_garage_colliders(block, pos, radius, collider_height, mousehole_side, wall_thickness, visualize);
+        } else {
+            spawn_solid_colliders(block, pos, radius, collider_height, visualize);
+        }
+    });
+}
+
+fn spawn_garage_colliders(building_parent : EntityId, base_pos : Vec3, radius : f32, height : f32, door_side : u8, wall_thickness : f32, visualize : bool ) {
+    for side in 0..4 {
+        if side == door_side%4 {
+            continue;
+        }
+        let wall = Entity::new()
+            .with(translation(), base_pos + (side_to_dir(side) * (radius - wall_thickness * 0.5) + vec3(0., 0., height*0.5)) )
+            .with(rotation(), side_to_rotation(side))
+            .with(scale(), vec3(radius+radius, wall_thickness, height))
+            .with(cube_collider(), vec3(1.,1.,1.))
+            .spawn();
+        entity::add_child(building_parent, wall);
+        if visualize {
+            entity::add_component(wall, cube(), ());
+        }
+    }
+}
+
+fn spawn_solid_colliders(building_parent : EntityId, base_pos : Vec3, radius : f32, height : f32, visualize : bool ) {
+    let solid = Entity::new()
+        .with(translation(), base_pos + vec3(0., 0., height*0.5))
+        .with(scale(), vec3(radius+radius, radius*radius, height))
+        .with(cube_collider(), vec3(1.,1.,1.))
+        .spawn();
+    entity::add_child(building_parent, solid);
+    if visualize {
+        entity::add_component(solid, cube(), ());
+    }
+}
+
+fn side_to_dir(side : u8) -> Vec3 {
+    match side%4 {
+        0 => { vec3(1., 0., 0.) }
+        1 => { vec3(0., 1., 0.) }
+        2 => { vec3(-1., 0., 0.) }
+        3 => { vec3(0., -1., 0.) }
+        _ => { panic!() }
+    }
+}
+
+fn side_to_rotation(side : u8) -> Quat {
+    Quat::from_rotation_z((side%4 + 3) as f32 * PI * 0.5)
+}
---
+++ ww_gen/src/server.rs
@@ -0,0 +1,8 @@
+use ambient_api::prelude::*;
+
+mod s_block_collider;
+
+#[main]
+pub fn main() {
+    s_block_collider::setup();
+}
