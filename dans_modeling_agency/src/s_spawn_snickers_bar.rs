eaf3df66abef414164b8bf948dc6317722ac3b19
Alexander Martin <hi@droqen.com>
2023-08-09 13:05:02 -0400

    A SNICKERS BAR FOR SOME REASON

---
+++ dans_modeling_agency/src/s_spawn_snickers_bar.rs
@@ -0,0 +1,13 @@
+use ambient_api::{
+    core::{
+        model::components::model_from_url,
+        // 
+    },
+    prelude::*,
+};
+
+pub fn setup() {
+    Entity::new()
+        .with(model_from_url(), crate::dans_modeling_agency::assets::url("why a snickers bar tho.glb"))
+        .spawn();
+}
--- dans_modeling_agency/src/server.rs
+++ dans_modeling_agency/src/server.rs
@@ -1,12 +1,14 @@
 use ambient_api::prelude::*;
 
 mod s_adjust_nice_camera;
-mod s_spawn_quad;
-mod s_spawn_boat;
+// mod s_spawn_quad;
+// mod s_spawn_boat;
+mod s_spawn_snickers_bar;
 
 #[main]
 pub fn main() {
     s_adjust_nice_camera::setup();
-    s_spawn_quad::setup();
-    s_spawn_boat::setup();
+    // s_spawn_quad::setup();
+    // s_spawn_boat::setup();
+    s_spawn_snickers_bar::setup();
 }
