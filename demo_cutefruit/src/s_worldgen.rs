33ce96d2ccff1569ce21179916603d7c1a2acbb1
Alexander Martin <hi@droqen.com>
2023-08-13 11:24:50 -0400

    worldgen uses 'dan1' decor instead of 'cube' decor

--- demo_cutefruit/src/s_worldgen.rs
+++ demo_cutefruit/src/s_worldgen.rs
@@ -1,6 +1,6 @@
 use ambient_api::{prelude::*, core::transform::components::translation};
 
-use crate::embers::ww_gen::components::{block_size, block_door_side, block_decor_cube, };
+use crate::embers::ww_gen::components::{block_size, block_door_side, block_decor_dan1, };
 
 use crate::embers::demo_cutefruit::components::is_dot;
 
@@ -11,7 +11,7 @@ pub fn setup() {
             Entity::new()
                 .with(translation(), pos)
                 .with(block_size(), vec3(6.0, 6.0, 10.0))
-                .with(block_decor_cube(), ())
+                .with(block_decor_dan1(), ())
                 .spawn();
         } else {
             Entity::new()
@@ -20,23 +20,4 @@ pub fn setup() {
                 .spawn();
         }
     }}
-    // Entity::new()
-    //     .with(translation(), vec3(0., 0., 0.))
-    //     .with(block_size(), vec3(6.0, 6.0, 10.0))
-    //     .with(block_decor_cube(), ())
-    //     .spawn();
-
-    // Entity::new()
-    //     .with(translation(), vec3(6.0, -6.0, 0.))
-    //     .with(block_size(), vec3(6.0, 6.0, 10.0))
-    //     .with(block_door_side(), 0)
-    //     .with(block_decor_cube(), ())
-    //     .spawn();
-
-    // Entity::new()
-    //     .with(translation(), vec3(12.0, -12.0, 0.))
-    //     .with(block_size(), vec3(6.0, 6.0, 10.0))
-    //     .with(block_door_side(), 1)
-    //     .with(block_decor_cube(), ())
-    //     .spawn();
 }
