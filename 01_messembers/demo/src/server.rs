35714f4acbd2cf0e9186b57ea8f4edb8ac372d93
Alexander Martin <hi@droqen.com>
2023-08-17 12:09:59 -0400

    refactor/autoformat

--- 01_messembers/demo/src/server.rs
+++ 01_messembers/demo/src/server.rs
@@ -1,11 +1,5 @@
 use ambient_api::{
-    core::{
-        primitives::components::quad,
-        transform::{
-            // components::{lookat_target, translation},
-            concepts::make_transformable,
-        },
-    },
+    core::{primitives::components::quad, transform::concepts::make_transformable},
     prelude::*,
 };
 
@@ -18,12 +12,19 @@ pub async fn main() {
 
     sleep(1.).await; // necessary to allow embers to set themselves up before init messages are sent (so they're not missed!)
 
-    run_async(async{loop{
-        embers::shouter::messages::NewShout{key:0, pos:vec3(r(-2., 2.), r(-2., 2.), r(-1., 1.)), txtmsg:"Hello".to_string()}.send_local_broadcast(true);
-        sleep(random::<f32>()*0.5).await;
-    }});
+    run_async(async {
+        loop {
+            embers::shouter::messages::NewShout {
+                key: 0,
+                pos: vec3(r(-2., 2.), r(-2., 2.), r(-1., 1.)),
+                txtmsg: "Hello".to_string(),
+            }
+            .send_local_broadcast(true);
+            sleep(random::<f32>() * 0.5).await;
+        }
+    });
 }
 
-fn r(lowest:f32, highest:f32)->f32 {
-    return lowest + (highest-lowest)*random::<f32>();
+fn r(lowest: f32, highest: f32) -> f32 {
+    return lowest + (highest - lowest) * random::<f32>();
 }
