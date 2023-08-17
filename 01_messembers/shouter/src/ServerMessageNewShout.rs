7dc58e7a67bf91175765fe546af973ae7cef6018
Alexander Martin <hi@droqen.com>
2023-08-16 21:04:13 -0400

    shouts are stored in a parent object now

--- 01_messembers/shouter/src/ServerMessageNewShout.rs
+++ 01_messembers/shouter/src/ServerMessageNewShout.rs
@@ -5,19 +5,24 @@ use crate::embers::shouter::components::{
 };
 
 pub fn setup() {
-    crate::embers::shouter::messages::NewShout::subscribe(|_src,msg|{
-        Entity::new()
-            .with(name(), format!("Shout: \"{}\"", msg.key))
-            .with(shout_key(), msg.key)
-            .with(shout_prog(), 0.0)
-            .with(shout_base_pos(), msg.pos)
-            .with(shout_text_center(), vec3(msg.txtmsg.len() as f32 * 4., 15., 0.0))
-            .with(text(), msg.txtmsg)
-            .with(scale(), Vec3::ONE * 0.05)
-            .with(local_to_world(), Mat4::IDENTITY)
-            .with(mesh_to_local(), Mat4::IDENTITY)
-            .with(mesh_to_world(), Mat4::IDENTITY)
-            .with(main_scene(), ())
-            .spawn();
+    let shouts_parent = Entity::new()
+        .with(name(), "Shouts".to_string())
+        .spawn();
+
+    crate::embers::shouter::messages::NewShout::subscribe(move|_src,msg|{
+        entity::add_child(shouts_parent,
+            Entity::new()
+                .with(shout_key(), msg.key)
+                .with(shout_prog(), 0.0)
+                .with(shout_base_pos(), msg.pos)
+                .with(shout_text_center(), vec3(msg.txtmsg.len() as f32 * 4., 15., 0.0))
+                .with(text(), msg.txtmsg)
+                .with(scale(), Vec3::ONE * 0.05)
+                .with(local_to_world(), Mat4::IDENTITY)
+                .with(mesh_to_local(), Mat4::IDENTITY)
+                .with(mesh_to_world(), Mat4::IDENTITY)
+                .with(main_scene(), ())
+                .spawn()
+        );
     });
 }
